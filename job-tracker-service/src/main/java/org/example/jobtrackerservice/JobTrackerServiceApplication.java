24c3bd8f91befe1a695a4712c95c0404d707e6fd
malyga99 <mikhail.malygin.2024@gmail.com>
2025-05-18 15:02:59 +0500

    infra: add job-tracker-service infra

---
+++ job-tracker-service/src/main/java/org/example/jobtrackerservice/JobTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobtrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobTrackerServiceApplication.class, args);
+    }
+
+}
