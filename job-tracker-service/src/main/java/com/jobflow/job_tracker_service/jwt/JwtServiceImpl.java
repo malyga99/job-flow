75778988714fa3844c6f75d0bcc58d8556da2cac
malyga99 <mikhail.malygin.2024@gmail.com>
2025-05-18 17:43:27 +0500

    feat: add jwt token validation

---
+++ job-tracker-service/src/main/java/com/jobflow/job_tracker_service/jwt/JwtServiceImpl.java
@@ -0,0 +1,48 @@
+package com.jobflow.job_tracker_service.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtServiceImpl.class);
+
+    private final String SECRET_KEY;
+
+    public JwtServiceImpl(@Value("${jwt.secret-key}") String SECRET_KEY) {
+        this.SECRET_KEY = SECRET_KEY;
+    }
+
+    @Override
+    public Claims extractClaims(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        LOGGER.debug("Extracted claims from token for userId: {}", claims.getSubject());
+        return claims;
+    }
+
+    @Override
+    public String extractUserId(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    @Override
+    public Key getSecretKey() {
+        byte[] decodeKey = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(decodeKey);
+    }
+}
---
+++ job-tracker-service/src/test/java/com/jobflow/job_tracker_service/jwt/JwtAuthenticationFilterTest.java
@@ -0,0 +1,106 @@
+package com.jobflow.job_tracker_service.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthenticationFilterTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private FilterChain filterChain;
+
+    @Mock
+    private JwtService jwtService;
+
+    @InjectMocks
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @BeforeEach
+    public void setup() {
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    public void doFilterInternal_withoutAuthHeader_skipFilter() throws ServletException, IOException {
+        when(request.getHeader("Authorization")).thenReturn(null);
+
+        authenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+        verifyNoInteractions(jwtService);
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    public void doFilterInternal_notJwtTokenInHeader_skipFilter() throws ServletException, IOException {
+        when(request.getHeader("Authorization")).thenReturn("Notbearer " + "token");
+
+        authenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+        verifyNoInteractions(jwtService);
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    public void doFilterInternal_userIdIsNull_skipFilter() throws ServletException, IOException {
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + "token");
+        when(jwtService.extractUserId("token")).thenReturn(null);
+
+        authenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    public void doFilterInternal_userAlreadyAuthenticated_skipFilter() throws ServletException, IOException {
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + "token");
+        when(jwtService.extractUserId("token")).thenReturn("1");
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+
+        authenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    public void doFilterInternal_tokenIsValid_setAuthentication() throws ServletException, IOException {
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + "token");
+        when(jwtService.extractUserId("token")).thenReturn("1");
+
+        authenticationFilter.doFilterInternal(request, response, filterChain);
+
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+        assertEquals(SecurityContextHolder.getContext().getAuthentication().getName(), "1");
+        verify(filterChain, times(1)).doFilter(request, response);
+    }
+}
